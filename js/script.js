cd76eaa8d10132528760344ef76a21c02f2a1eaf
Herviantryp <herviantryputera@gmail.com>
2023-10-26 21:06:20 +0700

    beres

--- js/script.js
+++ js/script.js
@@ -1,34 +1,10 @@
-// function replaceName () {
-//     let name = prompt("Halo, Dengan siapakah ini?","");
-//     document.getElementById("name").innerHTML = name;
-// }
-
-// document.getElementById('tombol').addEventListener("click", function() {
-//     replaceName();
-    
-// })
-// replaceName();
-
-// let nameInput = document.getElementById("name-input");
-
-// document.getElementById('submit').addEventListener("click", function() {
-//     const name = nameInput.value
-
-//     if (name == ""){
-//         document.getElementById("error-name").innerHTML = "Nama Harus Diisi"
-//     } else {
-//     document.getElementById('name').innerHTML = name;
-//     }
-// })
-
 function validateForm(){
     const nama = document.forms["message-form"]["nama"].value;
     const birthday =  document.forms["message-form"]["birthday"].value;
     const gender = document.forms["message-form"]["gender"].value;
     const messages =  document.forms["message-form"]["messages"].value;
     if (nama == "" || birthday == "" || gender == "" || messages == ""){
-        document.getElementById("error").innerHTML = "tidak boleh ada yang kosong"
-
+        alert("Tidak Boleh Ada Yang Kosong")
         return false
     }
 
